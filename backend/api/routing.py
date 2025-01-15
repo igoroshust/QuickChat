c3fe24647851b90ff1346199de520c2f82f69d83
Igor <igoroshust75@gmail.com>
2025-01-16 01:59:48 +0900

    feat: add debug messages

--- backend/api/routing.py
+++ backend/api/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/api/personal-chat/(?P<username>\w+)/$', consumers.PersonalChatConsumer.as_asgi()),  # Личный чат
-    re_path(r'ws/api/group-chat/(?P<room_name>\w+)/$', consumers.GroupChatConsumer.as_asgi()),
+    re_path(r'ws/api/chats/(?P<username>\w+)/$', consumers.PersonalChatConsumer.as_asgi()),  # personal-chat
+    re_path(r'ws/api/groups/(?P<room_name>\w+)/$', consumers.GroupChatConsumer.as_asgi()), # group-chat
 ]
